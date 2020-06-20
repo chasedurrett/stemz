b543325afdcabda42c3e1c008ca77892ddf2f5d2
chasedurrett <chase.durrett80@gmail.com>
2020-06-20 01:02:09 -0500

    working on sample waveforms

--- src/components/Sample/Sample.js
+++ src/components/Sample/Sample.js
@@ -1,12 +1,31 @@
 import React from "react";
+import ReactPlayer from "react-player";
+import WaveSurfer from 'wavesurfer'
 
 function Sample(props) {
-  const { name } = props;
-  console.log(props)
+  const {
+    id,
+    name,
+    url,
+    sampleType,
+    sampleKey,
+    instrument,
+    genre,
+    author,
+  } = props;
+  console.log(props);
   return (
     <div>
       sample
-      <h2>{name}</h2>
+      <h4>{name}</h4>
+      <h4>{instrument}</h4>
+      <h4>{genre}</h4>
+      <h4>{sampleKey}</h4>
+      <h4>{sampleType}</h4>
+      <h4>{author}</h4>
+      {/*<ReactPlayer id="audio-element2" controls={true} width={50} height={30} url={url} />*/}
+      <audio id="audio-element" src={url} controls={true}></audio>
+      
       <button>Download</button>
     </div>
   );
