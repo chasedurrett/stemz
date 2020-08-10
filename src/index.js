16794d42108c827133d6b756ae7467a163d9318b
Chase Durrett <donut@pop-os.localdomain>
2020-08-09 22:59:07 -0500

    new build

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { HashRouter, BrowserRouter } from "react-router-dom";
-const Router = process.env.NODE_ENV === 'development' ? HashRouter : BrowserRouter
 import { Provider } from "react-redux";
 import store from "./dux/store";
+const Router = process.env.NODE_ENV === 'development' ? HashRouter : BrowserRouter 
 
 ReactDOM.render(
   <React.StrictMode>
